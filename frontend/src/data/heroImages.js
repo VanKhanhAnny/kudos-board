1981e2e688256e08ad94ad47d50eb015f26a9e37
Anny Dang <arianne.dangvankhanh@gmail.com>
2026-07-01 10:09:59 -0700

    change import from assets instead of public

--- frontend/src/data/heroImages.js
+++ frontend/src/data/heroImages.js
@@ -1,18 +1,35 @@
+import dsc03074 from '../assets/DSC03074.JPG'
+import dsc03130 from '../assets/DSC03130.JPG'
+import dsc03132 from '../assets/DSC03132.JPG'
+import dsc03151 from '../assets/DSC03151.JPG'
+import dsc03180 from '../assets/DSC03180.JPG'
+import dsc03212 from '../assets/DSC03212.JPG'
+import dsc03218 from '../assets/DSC03218.JPG'
+import dsc03251 from '../assets/DSC03251.JPG'
+import dsc03255 from '../assets/DSC03255.JPG'
+import img4573 from '../assets/IMG_4573.JPG'
+import img4575 from '../assets/IMG_4575.JPG'
+import img4576 from '../assets/IMG_4576.JPG'
+import img4596 from '../assets/IMG_4596.JPG'
+import img4660 from '../assets/IMG_4660.JPG'
+import img4668 from '../assets/IMG_4668.JPG'
+import img4680 from '../assets/IMG_4680.JPG'
+
 export const heroImages = [
-  '/DSC03074.JPG',
-  '/DSC03130.JPG',
-  '/DSC03132.JPG',
-  '/DSC03151.JPG',
-  '/DSC03180.JPG',
-  '/DSC03212.JPG',
-  '/DSC03218.JPG',
-  '/DSC03251.JPG',
-  '/DSC03255.JPG',
-  '/IMG_4573.JPG',
-  '/IMG_4575.JPG',
-  '/IMG_4576.JPG',
-  '/IMG_4596.JPG',
-  '/IMG_4660.JPG',
-  '/IMG_4668.JPG',
-  '/IMG_4680.JPG',
+  dsc03074,
+  dsc03130,
+  dsc03132,
+  dsc03151,
+  dsc03180,
+  dsc03212,
+  dsc03218,
+  dsc03251,
+  dsc03255,
+  img4573,
+  img4575,
+  img4576,
+  img4596,
+  img4660,
+  img4668,
+  img4680,
 ]
