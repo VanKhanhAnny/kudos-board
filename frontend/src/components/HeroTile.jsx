b3838b1a36c3e842014eb3123bf0dbe897fe826a
Anny Dang <arianne.dangvankhanh@gmail.com>
2026-07-01 11:38:14 -0700

    fix animation style of hero background

--- frontend/src/components/HeroTile.jsx
+++ frontend/src/components/HeroTile.jsx
@@ -1,28 +1,32 @@
 import { useMemo } from 'react'
 import './HeroTile.css'
 
-const ANIMATION_PROBABILITY = 0.2
+const AMBIENT_PROBABILITY = 0.2
+const INTRO_DURATION_MS = 2600
 
 export function HeroTile({ tile }) {
-  const { animates, delay, duration } = useMemo(() => {
+  const { animates, ambientDelay, ambientDuration, introDelay } = useMemo(() => {
     return {
-      animates: Math.random() < ANIMATION_PROBABILITY,
-      delay: Math.random() * 3000,
-      duration: 1500 + Math.random() * 2500,
+      animates: Math.random() < AMBIENT_PROBABILITY,
+      ambientDelay: Math.random() * 3000,
+      ambientDuration: 1500 + Math.random() * 2500,
+      introDelay: Math.random() * 1400,
     }
   }, [])
 
+  const style = {
+    '--intro-delay': `${introDelay}ms`,
+    '--intro-duration': `${INTRO_DURATION_MS}ms`,
+  }
+  if (animates) {
+    style['--ambient-delay'] = `${INTRO_DURATION_MS + ambientDelay}ms`
+    style['--ambient-duration'] = `${ambientDuration}ms`
+  }
+
   return (
     <div
-      className={`hero-tile ${animates ? 'is-animating' : ''}`}
-      style={
-        animates
-          ? {
-              animationDelay: `${delay}ms`,
-              animationDuration: `${duration}ms`,
-            }
-          : undefined
-      }
+      className={`hero-tile is-intro ${animates ? 'is-animating' : ''}`}
+      style={style}
     >
       {tile.imageUrl ? (
         <img src={tile.imageUrl} alt="" />
